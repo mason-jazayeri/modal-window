6b1f503fc539b909e79297621d6c70d6661c2e26
mason.jazayeri@gmail.com <mason.jazayeri@gmail.com>
2025-02-08 18:10:54 +0330

    Fixed the bug of clicking above and bellow modal box. Made some improvements

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const showModalBtns = document.querySelectorAll('.show-modal');
 const closeModalBtn = document.querySelector('.close-modal');
-const modalContainerEl = document.querySelector('.modal-container');
+const modalEl = document.querySelector('.modal');
 const overlayEl = document.querySelector('.overlay');
 
 // element.classList.add('my-class');
@@ -10,21 +10,21 @@ const overlayEl = document.querySelector('.overlay');
 // element.classList.toggle('my-class');
 // element.classList.contains('my-class');
 
-// document.querySelectorAll('.my-class') → returns an array containing all the elements with that class. Then we can access all of them with a loop
+// document.querySelectorAll('.my-class') → returns an array-like containing all the elements with that class. Then we can access all of them with a loop
 
 const closeModal = () => {
-  modalContainerEl.classList.add('hidden');
+  modalEl.classList.add('hidden');
   overlayEl.classList.add('hidden');
 };
 
 const openModal = () => {
-  modalContainerEl.classList.remove('hidden');
+  modalEl.classList.remove('hidden');
   overlayEl.classList.remove('hidden');
 };
 
-for (let i = 0; i < showModalBtns.length; i++) {
-  showModalBtns[i].addEventListener('click', openModal);
-}
+showModalBtns.forEach(item => {
+  item.addEventListener('click', openModal);
+});
 
 closeModalBtn.addEventListener('click', closeModal);
 
@@ -32,8 +32,5 @@ overlayEl.addEventListener('click', closeModal);
 
 // Note the keydown event
 document.addEventListener('keydown', e => {
-  if (e.key === 'Escape' && !modalContainerEl.classList.contains('hidden'))
-    closeModal();
+  if (e.key === 'Escape' && !modalEl.classList.contains('hidden')) closeModal();
 });
-
-// BUG: When I click above and bellow the modal window, it'll be not removed.
